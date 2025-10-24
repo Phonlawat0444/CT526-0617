69f76b96e3002eef65f326e2dd73f3854ba65e18
Phonlawat0444 <68130617@dpu.ac.th>
2025-10-24 11:31:08 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from mylib import myfunc
-turns = input("How many turns you want to run [ ] :")
-for i in range(1 , turns + 1):
-    print(myfunc("x",i))
+
+def main():
+    turns = int(input("How many turns you want to run [4]: ") or 4)
+    for i in range(1, turns + 1):
+        print(myfunc("x", i))
+
+if __name__ == "__main__":
+    main()
